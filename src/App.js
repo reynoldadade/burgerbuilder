081b2dd9e0a05763c171494c5aeda53f1bb283ef
Reyold Osei Adade <reynoldadade@gmail.com>
2020-03-10 09:33:30 +0000

    created layout component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import './App.css';
+import Layout from './components/Layout/Layout';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
