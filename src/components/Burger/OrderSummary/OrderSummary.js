2de7fdd8ed6b4841e103fbdd020cc9bfd4853389
Reyold Osei Adade <reynoldadade@gmail.com>
2020-03-11 12:02:38 +0000

    changed file structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,20 +3,21 @@ import Aux from '../../../hoc/Auxilliary';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-  ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: 'capitalize' }}> {igKey}</span>:
-        {this.props.ingredients[igKey]}
-      </li>
-    );
-  });
+  //this could be a funcitonal component, could be a class
   render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: 'capitalize' }}> {igKey}</span>:
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
     return (
       <Aux>
         <h3>Your Order</h3>
         <p>A delicous burger with the following ingredients:</p>
-        <ul>{this.ingredientSummary}</ul>
+        <ul>{ingredientSummary}</ul>
         <p>
           <strong>Total Price : {this.props.price.toFixed(2)}</strong>
         </p>
