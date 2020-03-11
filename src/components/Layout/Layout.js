0b90a861dbc8a28641caf686bfcb9cd47b97f225
Reyold Osei Adade <reynoldadade@gmail.com>
2020-03-11 08:34:55 +0000

    Added logo preparing sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Auxilliary';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = props => (
   <Aux>
-    <div>Toolbar, sidedrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,4 @@
+import React from 'react';
+const sideDrawer = props => {
+  return <div></div>;
+};
